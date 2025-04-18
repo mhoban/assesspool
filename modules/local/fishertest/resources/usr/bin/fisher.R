f79195755b8b116a80e8a4957fcccb24cfdf67f8
mykle hoban <mhoban@hawaii.edu>
2025-04-19 11:31:56 +1200

    fisher test module

--- modules/local/fishertest/resources/usr/bin/fisher.R
+++ modules/local/fishertest/resources/usr/bin/fisher.R
@@ -6,7 +6,7 @@ suppressPackageStartupMessages(library(readr))
 suppressPackageStartupMessages(library(janitor))
 suppressPackageStartupMessages(library(purrr))
 suppressPackageStartupMessages(library(optparse))
-# matrixStats
+suppressPackageStartupMessages(library(matrixStats))
 
 options(dplyr.summarise.inform = FALSE)
 
@@ -71,10 +71,20 @@ expand_callback <- function(opt, flag, val, parser, args, ...) {
   char.expand(val,args)
 }
 
+pops_callback <- function(opt, flag, val, parser, ...) {
+  if (file.exists(val)) {
+    read_lines(file)
+  } else {
+    unlist(strsplit(val,","))
+  }
+}
+
 
 # set up option list
 option_list <- list(
-    make_option(c("-m", "--window-type"), action="callback", default="single", type='character', help="Window type", 
+    make_option(c("-p", "--pools"), action="callback", default=NA, type='character',
+                help="Subset of pools for which to calculate Fisher test (comma-separated or listed in a text file)",callback=pops_callback),
+    make_option(c("-m", "--window-type"), action="callback", default="single", type='character', help="Window type",
                 callback=expand_callback, callback_args = list(args=c("single","interval","queue"))),
     make_option(c("-w", "--window-size"), action="store", default=1, type='double', help="Fst window size"),
     make_option(c("-s", "--window-step"), action="store", default=0, type='double', help="Fst window step"),
@@ -85,32 +95,12 @@ option_list <- list(
     make_option(c("-A", "--all-snps"), action="store_true", default=FALSE, type='logical', help="Save fisher test results for all SNPs, regardless of window size"),
     make_option(c("-j", "--adjust-pval"), action="store_true", default=FALSE, type='logical', help="Adjust p-value for multiple comparisons"),
     make_option(c("-J", "--adjust-method"), action="store", default="bonferroni", type='character', help="P-value adjustment method"),
-    make_option(c("-p", "--prefix"), action="store", default="", type='character', help="Output file prefix"),
-    make_option(c("-o", "--output-dir"), action="store", default=getwd(), type='character', help="Output directory"),
+    make_option(c("-f", "--prefix"), action="store", default="", type='character', help="Output file prefix"),
+    make_option(c("-o", "--output-dir"), action="store", default=".", type='character', help="Output directory"),
     make_option(c("-t", "--threads"), action="store", default=1, type='double', help="Number of threads"),
     make_option(c("-l", "--lines-per-thread"), action="store", default=1e06, type='double', help="Number of lines to load per thread")
 )
 
-# debug_args <- c(
-#   '--min-coverage','10',
-#   '--max-coverage','1000',
-#   '--min-count','2',
-#   '--window-type','interval',
-#   '--window-size','100',
-#   '--prefix','fisher',
-#   "--output-dir","/Users/deadbilly/projects/pipeline/pool/nf-core-assesspool/testing/test/fisher",
-#   "/Users/deadbilly/projects/pipeline/pool/nf-core-assesspool/testing/test/fisher/freq.csv"
-# )
-
-# debug_args <- c(
-#   '--min-coverage','10',
-#   '--min-count','2',
-#   '--window-type','interval',
-#   '--window-size','100',
-#   '--prefix','test2',
-#   '--output-dir','../nf-core-assesspool/testing/test/fisher/',
-#   '../nf-core-assesspool/testing/test/fisher/freq.csv'
-# )
 
 # use debug arguments if we have 'em
 if (exists('debug_args')) {
@@ -150,20 +140,57 @@ p_combine     <- opt$options$pval_aggregate
 save_all      <- opt$options$all_snps
 adjust_p      <- opt$options$adjust_pval
 adjust_method <- opt$options$adjust_method
-
+pools          <- opt$options$pools
+if (!all(is.na(pools))) {
+  if (length(pools) != 2) {
+    stop("--pools must be a list of exactly two pool names, either comma-separated or in a text file")
+  }
+}
 
 if (!dir.exists(outdir)) {
   stop("Output directory does not exist.")
 }
 
+if (!file.exists(opt$args[1])) {
+  stop(sprintf("Frequency file %s does not exist.",opt$args[1]))
+}
 
 # filter frequency table down to just what we'd consider snps
 # this filtering should match popoolation/poolfst
-freq_snps <- read_csv(opt$args[1],col_types = cols(),progress = FALSE) %>%
+
+# split(ceiling(seq_along(.)/50)) %>%
+  
+freq_snps <- read_csv(opt$args[1],col_types = cols(),progress = FALSE) 
+
+if (all(is.na(pools))) {
+  pools <- freq_snps %>%
+    slice(1) %>%
+    pivot_longer(-c(CHROM:ALT,starts_with("TOTAL",ignore.case = FALSE)),names_to = c("pool","measure"),values_to="count",names_pattern = "^(.+)\\.([^.]+)$") %>%
+    pull(pool) %>%
+    sort() %>%
+    unique()
+}
+
+# make sure we're dealing with two pools
+npool <- length(pools)
+# and bail if we're not
+if (npool != 2) {
+  stop("Frequency file must contain exactly two pools")
+}
+
+freq_snp_og <- freq_snps
+# continue filtering
+freq_snps <- freq_snp_og %>%
+  select(CHROM:ALT,starts_with(paste0(pools,"."),ignore.case = FALSE)) %>%
   mutate(
-    lwr = floor((POS-1)/window_step)*window_step,
-    upr = lwr+window_size,
-    middle=floor((upr+1+lwr)/2)
+    `TOTAL.REF_CNT` = rowSums(pick(ends_with(".REF_CNT",ignore.case = FALSE))),
+    `TOTAL.ALT_CNT` = rowSums(pick(ends_with(".ALT_CNT",ignore.case = FALSE))),
+    `TOTAL.DEPTH` = rowSums(pick(ends_with(".DEPTH",ignore.case = FALSE))),
+  ) %>%
+  mutate(
+    lwr = floor((POS-1)/window_step)*window_step+1,
+    upr = lwr+window_size-1,
+    middle=floor((upr+lwr)/2)
   ) %>%
   filter( if_all(ends_with(".DEPTH"),~.x >= min_depth) ) %>%
   add_count(CHROM,middle,name="snp_count") %>%
@@ -179,56 +206,55 @@ freq_snps <- read_csv(opt$args[1],col_types = cols(),progress = FALSE) %>%
 fisher_results <- freq_snps %>%
   select(order(colnames(.))) %>%
   select(
-    chrom, pos, middle, snp_count,
+    chrom, pos, middle, snp_count,start=lwr,end=upr,
     ends_with(".DEPTH",ignore.case = FALSE),
     ends_with(".REF_CNT",ignore.case = FALSE),
     ends_with(".ALT_CNT",ignore.case = FALSE)
-  ) %>% 
+  ) %>%
   pivot_longer(
-    -c(chrom,pos,middle,snp_count,ends_with(".DEPTH",ignore.case = FALSE)),
+    -c(chrom,pos,middle,snp_count,start,end,ends_with(".DEPTH",ignore.case = FALSE)),
     names_to = c("pop","measure"),
     values_to = "count",
     names_pattern = "^(.+)\\.([^.]+)$"
   ) %>%
-  group_by(chrom,pos,middle,snp_count,across(ends_with(".DEPTH",ignore.case = FALSE))) %>%
+  group_by(chrom,pos,middle,start,end,snp_count,across(ends_with(".DEPTH",ignore.case = FALSE))) %>%
   summarise(pval = fisher.test(matrix(count,ncol=2))$p.value) %>%
   ungroup() %>%
-  mutate(min_cov = matrixStats::rowMins(as.matrix(pick(ends_with(".DEPTH",ignore.case = FALSE))))) 
+  mutate(min_cov = matrixStats::rowMins(as.matrix(pick(ends_with(".DEPTH",ignore.case = FALSE)))))
 
 if (adjust_p) {
   fisher_results <- fisher_results %>%
     mutate(p_adj = p.adjust(pval,method=adjust_method))
 }
 
-if (save_all | window_size == 1) {
-  ss <- sprintf("%s/%s_all_snps_fisher.tsv",outdir,file_prefix)
-  fisher_results %>% 
+
+pool_col <- paste0(paste0(pools,collapse=":"),".fisher")
+
+if (save_all & window_size > 1) {
+  ss <- sprintf("%s/%s_%s_all_snps_fisher.tsv",outdir,file_prefix,paste0(pools,collapse="-"))
+  fisher_results %>%
     mutate(
-      pval = -log10(pval),
+      "{pool_col}" := -log10(pval),
       snps = 1,
       covered_fraction = 1,
       start=pos,
       end=pos,
     ) %>%
-    select(chrom,start,middle=pos,end,snps,covered_fraction,avg_min_coverage = min_cov,pval) %>%
-    write_tsv(ss) 
+    select(chrom,start,middle=pos,end,snps,covered_fraction,avg_min_coverage = min_cov,all_of(pool_col)) %>%
+    write_tsv(ss)
 }
 
-if (window_size > 1) {
-  fisher_window <- fisher_results %>%
-    group_by(chrom,middle,snp_count) %>%
-    summarise(
-      pval = -log10(p_combine(pval)),
-      start = min(pos),
-      end = max(pos),
-      snps = n(),
-      covered_fraction = unique(snp_count)/window_size,
-      avg_min_coverage = mean(min_cov)
-    ) %>%
-    select(chrom,start,middle,end,snps,covered_fraction,avg_min_coverage,pval)  %>%
-    ungroup()
-  ss <- sprintf("%s/%s_all_snps_fisher.tsv",outdir,file_prefix)
-  write_tsv(fisher_window,ss)
-}
+fisher_results <- fisher_results %>%
+  group_by(chrom,middle,start,end,snp_count) %>%
+  summarise(
+    "{pool_col}" := -log10(p_combine(pval)),
+    snps = n(),
+    covered_fraction = unique(snp_count)/window_size,
+    avg_min_coverage = mean(min_cov)
+  ) %>%
+  select(chrom,start,middle,end,snps,covered_fraction,avg_min_coverage,all_of(pool_col))  %>%
+  ungroup()
 
-
+ss <- sprintf("%s/%s_%s_window_%d_%d_fisher.tsv",outdir,file_prefix,paste0(pools,collapse="-"),window_size,window_step)
+write_tsv(fisher_results,ss)
+# hello
